f25c9c361e29512e00351391c2defe5be5175c96
Dominik Żegleń <flesz3@o2.pl>
2021-01-20 17:33:50 +0100

    Add lock span to request trace

--- middleware/sim.go
+++ middleware/sim.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/dominik-zeglen/aquarium/sim"
+	opentracing "github.com/opentracing/opentracing-go"
 )
 
 // SimContextKey defines key holding website config data in request context
@@ -14,8 +15,10 @@ const SimContextKey = ContextKey("config")
 func WithSim(next http.Handler, s *sim.Sim) http.Handler {
 	return http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
+			simSpan, _ := opentracing.StartSpanFromContext(r.Context(), "lock")
 			ctx := context.WithValue(r.Context(), SimContextKey, s)
 			s.Lock()
+			simSpan.Finish()
 			defer s.Unlock()
 
 			next.ServeHTTP(w, r.WithContext(ctx))
